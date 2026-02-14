281a12de2efa0e7a3db110fdc929a373e1a07025
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-02-14 19:51:45 +0100

    use go-lint composite action

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,6 +9,19 @@ import (
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Inspect postgres schemas via a TUI.
+
+schemas needs the following environment variables to be set:
+- DATABASE_ADDRESS 
+- DATABASE_PORT
+- DATABASE_USERNAME
+- DATABASE_PASSWORD
+- DATABASE_DBNAME
+`)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
 	err := cmd.Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
