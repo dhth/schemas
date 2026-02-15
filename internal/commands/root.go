187bd45457dcc76e5ba0943294b17715eab331db
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-02-15 08:22:28 +0100

    use compose action for lint

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -1,11 +1,29 @@
 package cmd
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/dhth/schemas/internal/db"
 	"github.com/dhth/schemas/internal/ui"
 )
 
 func Execute() error {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Inspect postgres schemas via a TUI.
+
+schemas needs the following environment variables to be set:
+- DATABASE_ADDRESS 
+- DATABASE_PORT
+- DATABASE_USERNAME
+- DATABASE_PASSWORD
+- DATABASE_DBNAME
+`)
+		flag.PrintDefaults()
+	}
+	flag.Parse() // nolint
+
 	dbPool, err := db.CreateDBPool()
 	if err != nil {
 		return err
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
@@ -9,19 +8,6 @@ import (
 )
 
 func main() {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, `Inspect postgres schemas via a TUI.
-
-schemas needs the following environment variables to be set:
-- DATABASE_ADDRESS 
-- DATABASE_PORT
-- DATABASE_USERNAME
-- DATABASE_PASSWORD
-- DATABASE_DBNAME
-`)
-		flag.PrintDefaults()
-	}
-	flag.Parse()
 	err := cmd.Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
